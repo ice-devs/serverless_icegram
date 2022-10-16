0814499079740687c2039e4894f7a23907222a69
ice-devs <othukeotubu@gmail.com>
2022-10-16 19:34:06 +0100

    feat: restructured project

---
+++ client/src/config.ts
@@ -0,0 +1,9 @@
+const apiId = '9tmsdw1opf'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+
+export const authConfig = {
+  domain: 'dev-3xb6q98a.us.auth0.com',
+  clientId: 'yv7LTN6uzYae7RJ6EPQfRm7Dy93QHWCj',
+  callbackUrl: 'http://localhost:3000/callback'
+}
